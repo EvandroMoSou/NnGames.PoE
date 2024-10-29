15b09dd23102905e7a3555bde0b83ed42b73a0eb
Evandro Mota de Souza <emotasouza@PRODAM.SP.GOV.BR>
2024-10-29 18:22:13 -0300

    att

---
+++ NnGames.PoE.Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using NnGames.PoE2.OpenDb.Models;
+using System.Text.Json;
+
+namespace NnGames.PoE.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var l = new List<Currency>();
+
+            //var m = new Currency();
+            //m.Name = "Armourer's Scrap";
+            //m.StackSize = 40;
+            //m.Description = "Improves the quality of an armour";
+            //l.Add(m);
+
+            //m = new Currency();
+            //m.Name = "Chaos Orb";
+            //m.StackSize = 20;
+            //m.Description = "Removes a random modifier and augments a rare item with a new random modifier";
+            //m.Note = "Right click this item then left click a rare item to applt it.";
+            //l.Add(m);
+
+            //Console.WriteLine(JsonSerializer.Serialize(l));
+
+            //var json = EmbeddedResourceUtil.ReadResource("NnGames.PoE2.OpenDb.Jsons.currency.json", Assembly.GetAssembly(typeof(Currency)));
+            //var l = JsonSerializer.Deserialize<List<Currency>>(json);
+
+            //Console.WriteLine(l);
+        }
+    }
+}
---
+++ NnGames.PoE.Test/Utils/EmbeddedResourceUtil.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace NnGames.PoE.Test.Utils;
+
+public static class EmbeddedResourceUtil
+{
+    public static string ReadResource(string resourceName, Assembly assembly)
+    {
+        using (Stream stream = assembly.GetManifestResourceStream(resourceName)!)
+        using (StreamReader reader = new StreamReader(stream))
+            return reader.ReadToEnd();
+    }
+
+    public static bool CheckResource(string resourceName, Assembly assembly)
+    {
+        var names = assembly.GetManifestResourceNames();
+        return names.Contains(resourceName);
+    }
+}
